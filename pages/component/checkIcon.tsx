d45ad17588542f535d2a3912f657216cc4389740
techsyhub <techsyhub@gmail.com>
2023-03-19 03:23:59 +0500

    update keys

--- pages/component/checkIcon.tsx
+++ pages/component/checkIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CheckIcon = () => {
   return (
-    <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="11"></circle><path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none"></path></svg>
+    <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="11"></circle><path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none"></path></svg>
   )
 }
 
